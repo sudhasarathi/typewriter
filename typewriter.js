08f7c563aa41f9a4fc69ed309d93a8326e848c4b
sudhasarathi <sudhav158@email.com>
2023-01-17 15:06:12 -0700

    animated type

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+let time = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time);
+  time += 50;
+}
+
+
